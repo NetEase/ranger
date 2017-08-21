f001d82f698ea1e009c573473f8ab11482d68592
hzlimin2 <hzlimin2@corp.netease.com>
2017-08-21 14:57:59 +0800

    remove unuseful import

--- security-admin/src/main/java/org/apache/ranger/common/RangerServicePoliciesCache.java
+++ security-admin/src/main/java/org/apache/ranger/common/RangerServicePoliciesCache.java
@@ -30,8 +30,6 @@ import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.ranger.plugin.util.ServicePolicies;
 
-import com.netease.bdms.service.params.ranger.RangerPolicyItem;
-
 import java.util.*;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.ReentrantLock;
